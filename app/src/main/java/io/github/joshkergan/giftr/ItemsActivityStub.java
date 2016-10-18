b830848729e807077cf3971499cfbaf3619ddd7f
Josh Kergan <joshkergan@gmail.com>
2016-10-17 21:11:37 -0400

    Added .Travis.yml for testing purposes.

---
+++ app/src/main/java/io/github/joshkergan/giftr/ItemsActivityStub.java
@@ -0,0 +1,11 @@
+package io.github.joshkergan.giftr;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Josh on 16/10/2016.
+ * Stub Activity for working with People Activity
+ */
+public class ItemsActivityStub extends AppCompatActivity{
+
+}
